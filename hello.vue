f12ea35bb85a03222af9f9647996144a80c70168
wroy1005 <879631107@qq.com>
2020-12-31 00:01:22 +0800

    Add files via upload

---
+++ hello.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return { msg: 'zbc', cc: '123', afd: 'adfsdf' };
+  },
+};
+</script>
+
+<template>
+  <div
+    v-for="a in 5"
+    :key="a"
+    class="hello"
+    cl1ass="hello"
+    cl4ass="hello"
+    cla1ss="hello"
+    cl2ass="hello"
+    cl3ass="hello"
+    cla4ss="hello"
+  >
+    {{ msg }}
+  </div>
+</template>
+
+<style></style>
---
+++ index.js
@@ -0,0 +1,4 @@
+const abc = '123';
+// const def = 22;
+
+console.log(abc);
